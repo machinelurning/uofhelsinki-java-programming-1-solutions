e4466f4cd1ea9bdb3748fdeafbba5438acf2e3c6
IK Manding <github@ianmanding.net>
2022-04-27 16:46:59 +0800

    Commit

---
+++ part04-Part04_01.YourFirstAccount/src/main/java/YourFirstAccount.java
@@ -0,0 +1,13 @@
+
+public class YourFirstAccount {
+
+    public static void main(String[] args) {
+        // Do not touch the code in Account.java
+        // Write your program here
+
+        Account iansAccount = new Account("Ian's account", 100.00);
+        iansAccount.deposit(20.00);
+        System.out.println(iansAccount);
+
+    }
+}
