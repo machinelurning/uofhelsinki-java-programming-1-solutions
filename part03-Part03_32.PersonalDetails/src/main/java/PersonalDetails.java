b9b9f5d42cfd1e6791f142179293e2d65f7bd3a5
IK Manding <github@ianmanding.net>
2022-04-27 12:18:47 +0800

    Commit PersonalDetails

--- part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
+++ part03-Part03_32.PersonalDetails/src/main/java/PersonalDetails.java
@@ -7,6 +7,34 @@ public class PersonalDetails {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int longestLength = 0;
+        int sum = 0;
+        int counter = 0;
+        String longestName = "";
+
+        while (true) {
+            String nameAndAge = scanner.nextLine();
+
+            if (nameAndAge.equals("")) {
+                break;
+            }
+
+            String[] split = nameAndAge.split(",");
+
+            int currentLength = split[0].length();
+
+            if (currentLength > longestLength) {
+                longestLength = currentLength;
+                longestName = split[0];
+            }
+
+            sum = sum + Integer.valueOf(split[1]);
+            counter++;
+
+        }
+
+        System.out.println("Longest name: " + longestName);
+        System.out.println("Average of the birth years: " + (sum * 1.0 / counter));
 
     }
 }
