88c28b6025f0866a19d5592096f6ecef48c87197
IK Manding <github@ianmanding.net>
2022-04-27 11:57:08 +0800

    Commit AgeOfTheOldest

--- part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
+++ part03-Part03_30.AgeOfTheOldest/src/main/java/AgeOfTheOldest.java
@@ -6,6 +6,25 @@ public class AgeOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int oldest = 0;
+
+        while (true) {
+            String nameAndAge = scanner.nextLine();
+
+            if (nameAndAge.equals("")) {
+                break;
+            }
+
+            String[] split = nameAndAge.split(",");
+            int currentAge = Integer.valueOf(split[1]);
+
+            if (currentAge > oldest) {
+                oldest = currentAge;
+            }
+
+        }
+
+        System.out.println("Age of the oldest: " + oldest);
 
     }
 }
