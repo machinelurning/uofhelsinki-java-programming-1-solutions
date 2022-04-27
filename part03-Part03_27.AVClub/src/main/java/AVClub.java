48d050e4769b361d5859906540e092c211228adc
IK Manding <github@ianmanding.net>
2022-04-27 11:30:01 +0800

    Commit AVClub

--- part03-Part03_27.AVClub/src/main/java/AVClub.java
+++ part03-Part03_27.AVClub/src/main/java/AVClub.java
@@ -6,6 +6,21 @@ public class AVClub {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String string = scanner.nextLine();
+
+            if (string.equals("")) {
+                break;
+            }
+
+            String[] split = string.split(" ");
+
+            for (String i : split) {
+                if (i.contains("av")) {
+                    System.out.println(i);
+                }
+            }
+        }
 
     }
 }
