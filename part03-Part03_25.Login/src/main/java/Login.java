a1147de89820bc86ab0a8d4a0347705f402181c1
IK Manding <github@ianmanding.net>
2022-04-27 11:16:11 +0800

    Commit Login

--- part03-Part03_25.Login/src/main/java/Login.java
+++ part03-Part03_25.Login/src/main/java/Login.java
@@ -6,5 +6,18 @@ public class Login {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter username: ");
+        String userName = scanner.nextLine();
+
+        System.out.println("Enter password: ");
+        String passWord = scanner.nextLine();
+
+        if ((userName.equals("alex") && passWord.equals("sunshine"))
+                || (userName.equals("emma") && passWord.equals("haskell"))) {
+            System.out.println("You have successfully logged in!");
+        } else {
+            System.out.println("Incorrect username or password!");
+        }
+
     }
 }
