29d714abed4329299281d2b081231bef8d780940
IK Manding <github@ianmanding.net>
2022-04-27 12:12:10 +0800

    Commit NameOfOldest

--- part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
+++ part03-Part03_31.NameOfTheOldest/src/main/java/NameOfTheOldest.java
@@ -6,6 +6,27 @@ public class NameOfTheOldest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String oldestName = "";
+        int oldestAge = 0;
+
+        while (true) {
+            String nameAndAge = scanner.nextLine();
+
+            if (nameAndAge.equals("")) {
+                break;
+            }
+
+            String[] split = nameAndAge.split(",");
+            int currentAge = Integer.valueOf(split[1]);
+
+            if (currentAge > oldestAge) {
+                oldestAge = currentAge;
+                oldestName = split[0];
+            }
+
+        }
+
+        System.out.println("Name of the oldest: " + oldestName);
 
     }
 }
