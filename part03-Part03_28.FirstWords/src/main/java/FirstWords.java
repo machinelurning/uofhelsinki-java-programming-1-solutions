283d6fc40c4ce765bc93c1674f7e248ce485c124
IK Manding <github@ianmanding.net>
2022-04-27 11:47:05 +0800

    Commit FirstWords and LastWords

--- part03-Part03_28.FirstWords/src/main/java/FirstWords.java
+++ part03-Part03_28.FirstWords/src/main/java/FirstWords.java
@@ -6,6 +6,17 @@ public class FirstWords {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            String string = scanner.nextLine();
+
+            if (string.equals("")) {
+                break;
+            }
+
+            String[] split = string.split(" ");
+            System.out.println(split[0]);
+
+        }
 
     }
 }
--- part03-Part03_29.LastWords/src/main/java/LastWords.java
+++ part03-Part03_29.LastWords/src/main/java/LastWords.java
@@ -5,7 +5,16 @@ public class LastWords {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String string = scanner.nextLine();
 
+            if (string.equals("")) {
+                break;
+            }
+
+            String[] split = string.split(" ");
+            System.out.println(split[split.length - 1]);
+        }
 
     }
 }
